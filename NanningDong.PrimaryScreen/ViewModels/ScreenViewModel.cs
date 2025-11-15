da221f8038cf723b7c6ece3f8d6b8ee254bc9e6e
denglihong2007 <98096191+denglihong2007@users.noreply.github.com>
2025-11-15 21:59:39 +0800

    添加南宁东站主要看板

---
+++ NanningDong.PrimaryScreen/ViewModels/ScreenViewModel.cs
@@ -0,0 +1,16 @@
+using CRSim.ScreenSimulator.ViewModels;
+﻿using CRSim.Core.Models;
+using CRSim.Core.Abstractions;
+namespace NanningDong.PrimaryScreen.ViewModels
+{
+    public class ScreenViewModel : BaseScreenViewModel
+    {
+        public ScreenViewModel(ITimeService timeService, ISettingsService settingsService)
+            : base(timeService, settingsService)
+        {
+            ItemsPerPage = 11;
+            ScreenCount = 1;
+            StationType = StationType.Departure;
+        }
+    }
+}
---
+++ NanningDong.PrimaryScreen/Views/ScreenView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using NanningDong.PrimaryScreen.ViewModels;
+using System.Windows.Controls;
+
+namespace NanningDong.PrimaryScreen.Views
+{
+    public partial class ScreenView : Page
+    {
+        public ScreenViewModel ViewModel { get; }
+        public ScreenView(ScreenViewModel viewModel)
+        {
+            InitializeComponent();
+            ViewModel = viewModel;
+            DataContext = viewModel;
+        }
+    }
+}
